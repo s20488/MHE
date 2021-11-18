a01798be1c9d66719ce2b22cbd3ea8fa14c8c793
Anastasiia Ponkratova <56721899+s20488@users.noreply.github.com>
2021-11-18 14:24:59 +0200

    Add files via upload

---
+++ lab2/main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Struktura przedmiotu, który przechowuje wagę i odpowiednią wartość przedmiotu
+struct Item {
+    int value, weight;
+
+    Item(int value, int weight)
+    {
+        this->value=value;
+        this->weight=weight;
+    }
+};
+
+vector<Item> solution = {}; //Solution vector
+
+//Główna funkcja do rozwiązania problemu
+double knapsack(int W, vector<Item> solution, int n) {
+    int curWeight = 0; //Current weight in knapsack
+    double finalvalue = 0.0; //Result (value in Knapsack)
+
+    //Pętla przez wszystkie pozycje
+    for (int i = 0; i < n; i++) {
+        //Jeśli dodanie Item nie spowoduje przepełnienia, dodaje ją
+        if (curWeight + solution[i].weight <= W) {
+            curWeight += solution[i].weight;
+            finalvalue += solution[i].value;
+        }
+    }
+
+    //Zwracanie wartości końcowej
+    return finalvalue;
+}
+
+//Pobieranie danych z pliku
+void load_file(string filename) {
+    ifstream filepath(filename);
+    int v, w;
+
+    while(filepath >> v >> w) {
+        solution.push_back({v, w});
+    }
+}
+
+void print_data() {
+    //Nowa kolejność pozycji wraz z ich proporcjami
+    cout << "Value and weight of each item: " << endl;
+    for (int i = 0; i < solution.size(); i++) {
+        cout << solution[i].value << "  " << solution[i].weight << endl;
+    }
+}
+
+int main() {
+    int W = 50; //Waga plecaka
+
+    int n = sizeof(solution) / sizeof(solution[0]); //Rozmiar wektora
+
+    load_file("C:\\Users\\user\\CLionProjects\\BackPack\\dane.txt");
+
+    print_data();
+
+    //Wywołanie funkcji
+    cout << "Maximum value we can obtain = " << knapsack(W, solution, n);
+    return 0;
+}
